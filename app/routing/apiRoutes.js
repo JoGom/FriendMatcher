a12738f090ce577dbd3a298448b28a529c94490f
JoGom <jogoram428@gmail.com>
2019-02-12 12:01:03 -0700

    created routes for api

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,23 @@
+//links to the files that holds the friends data array
+const friendData = require("..data/friends.js");
+
+module.exports = function(app) {
+    
+    //shows friend data as JSON on the page
+    app.get("/api/friends", function(req, res){
+        res.json(friendData);
+    });
+
+    //uses body parsing middleware to add new items to array based on new entries
+    app.post("/api/friends", function(req, res){
+        friendData.push(req.body);
+    });
+
+    app.post("/api/clear", function(req, res) {
+        // Empty out the arrays of data
+        friendData = [];
+    });
+    
+};
+
+
